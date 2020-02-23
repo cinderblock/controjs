d63d3986d2a577974c90f3205d1c11fdcce0fa38
Cameron Tacklind <cameron@tacklind.com>
2020-02-23 01:03:00 -0800

    Cleanup

--- src/systems/makeCartSystem.ts
+++ src/systems/makeCartSystem.ts
@@ -1,28 +1,21 @@
 import { matrix } from 'mathjs';
 import { System } from './System';
 
-export function makeCart({
-  d = 1,
-  m = 1,
-  M = 5,
-  L = 2,
-  s = 'up',
-  g = -9.8,
-}: {
+type Options = {
   /**
    * Damping on ẋ
    */
-  d?: number;
+  d: number;
 
   /**
    * Mass of pendulum
    */
-  m?: number;
+  m: number;
 
   /**
    * Mass of cart
    */
-  M?: number;
+  M: number;
 
   /**
    * Length of pendulum
@@ -30,14 +23,7 @@ export function makeCart({
    * @units m
    * @default
    */
-  L?: number;
-
-  /**
-   * Starting condition
-   *
-   * @default up
-   */
-  s?: 'up' | 'down';
+  L: number;
 
   /**
    * Gravity
@@ -45,31 +31,49 @@ export function makeCart({
    * @default 9.8
    * @units m/s²
    */
-  g?: number;
-} = {}): System {
+  g: number;
+
+  /**
+   * Starting condition
+   *
+   * @default up
+   */
+  s: 'up' | 'down';
+};
+
+export function makeCart({
+  d = 1,
+  m = 1,
+  M = 5,
+  L = 2,
+  s = 'up',
+  g = -9.8,
+}: Partial<Options> = {}): System {
   if (s !== 'up') L = -L;
   const ML = M * L;
 
+  const a = -d / M;
+  const b = (-m * g) / M;
+  const c = -d / ML;
+  const e = (-(m + M) * g) / ML;
+
+  const n = 1 / M;
+  const o = 1 / ML;
+
   return {
-    A: matrix(
-      // prettier-ignore
-      [
-        [0, 1      ,                 0, 0],
-        [0, -d / M , - m      * g / M , 0],
-        [0,       0,                 0, 1],
-        [0, -d / ML, -(m + M) * g / ML, 0],
-      ],
-    ),
+    A: matrix([
+      [0, 1, 0, 0], // x
+      [0, a, b, 0], // ẋ
+      [0, 0, 0, 1], // θ
+      [0, c, e, 0], // ω
+    ]),
     states: 4,
-    B: matrix(
-      // prettier-ignore
-      [
-        [0     ],
-        [1 / M ],
-        [0     ],
-        [1 / ML],
-      ],
-    ),
+    B: matrix([
+      [0], // x
+      [n], // ẋ
+      [0], // θ
+      [o], // ω
+    ]),
     inputs: 1,
   };
 }

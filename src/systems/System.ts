c2778d22476442a92b81e2cfd2b1cb8c3a7a77b6
Cameron Tacklind <cameron@tacklind.com>
2020-02-23 22:25:00 -0800

    Comments good

--- src/systems/System.ts
+++ src/systems/System.ts
@@ -5,7 +5,19 @@ export type System = {
   B: Matrix;
   C?: Matrix;
   D?: Matrix;
+
+  /**
+   * The number of system states that we're tracking
+   */
   states: number;
+
+  /**
+   * The number of control inputs to the system
+   */
   inputs: number;
+
+  /**
+   * The number of observable values on our system
+   */
   observables?: number;
 };

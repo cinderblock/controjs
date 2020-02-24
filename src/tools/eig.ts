3bb215555b97f81ff805d615bb36db10a3af88db
Cameron Tacklind <cameron@tacklind.com>
2020-02-23 16:44:14 -0800

    Implement eig

--- src/tools/eig.ts
+++ src/tools/eig.ts
@@ -1,12 +1,18 @@
-import { Matrix } from 'mathjs';
+import * as math from 'mathjs';
+import { Matrix, matrix } from 'mathjs';
+
+// Type information for eigs function is completely missing
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const eigs = ((math as unknown) as any).eigs as (
+  mat: Matrix,
+) => { values: Matrix; vectors: Matrix };
 
 /**
- * Compute eigenvalues of a matrix.
+ * Compute eigenvalues for a square matrix.
  *
- * @param {Matrix} mat - Matrix to compute eigenvalues of
- * @returns {Matrix} The eigenvalues
+ * @param {Matrix} mat - Matrix to compute eigenvalues for
+ * @returns {Matrix} Eigenvalues as vector
  */
 export function eig(mat: Matrix): Matrix {
-  // TODO: implement
-  return mat;
+  return matrix(eigs(mat).values);
 }

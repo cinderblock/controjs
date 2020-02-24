da5dffde1a158bb56905a33acbfd910ac4ec23dc
Cameron Tacklind <cameron@tacklind.com>
2020-02-23 22:24:43 -0800

    Initial controllability implementation

--- src/tools/controllability.ts
+++ src/tools/controllability.ts
@@ -1,6 +1,13 @@
-import { Matrix, cross, matrix } from 'mathjs';
+import { Matrix, matrix, pow, multiply, squeeze } from 'mathjs';
 
 export function controllability({ A, B }: { A: Matrix; B: Matrix }): Matrix {
-  // TODO: implement
-  return matrix(cross(A, B));
+  const n = A.size().length;
+
+  const ret: number[][] = [];
+
+  for (let i = 0; i < n; i++) {
+    ret.push(squeeze(multiply(pow(A, i), B) as Matrix).toArray() as number[]);
+  }
+
+  return matrix(ret);
 }
